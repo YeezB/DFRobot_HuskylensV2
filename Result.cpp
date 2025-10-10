bb1b990b7dcd0be6a0c30bec3ea4bd4f0a716457
ouki.wang <ouki.wang@dfrobot.com>
2025-10-10 18:50:10 +0800

    解决读取int16数据内存不对其引发崩溃的bug

--- Result.cpp
+++ Result.cpp
@@ -40,7 +40,7 @@ Result::Result(const void *buf) {
 Result::~Result() {
 }
 #ifdef LARGE_MEMORY
-
+#define READ_INT16(p) ({ int16_t v; memcpy(&v, p, sizeof(v)); p += sizeof(v); v; })
 FaceResult::FaceResult(const void *buf) : Result(buf) {
     DBG("\n");
     PacketHead_t *head     = (PacketHead_t *)buf;
@@ -56,16 +56,18 @@ FaceResult::FaceResult(const void *buf) : Result(buf) {
         DBG("no externed data");
         return;
     }
-    leye_x   = *face_data++;
-    leye_y   = *face_data++;
-    reye_x   = *face_data++;
-    reye_y   = *face_data++;
-    nose_x   = *face_data++;
-    nose_y   = *face_data++;
-    lmouth_x = *face_data++;
-    lmouth_y = *face_data++;
-    rmouth_x = *face_data++;
-    rmouth_y = *face_data++;
+    
+
+    leye_x   = READ_INT16(face_data);
+    leye_y   = READ_INT16(face_data);
+    reye_x   = READ_INT16(face_data);
+    reye_y   = READ_INT16(face_data);
+    nose_x   = READ_INT16(face_data);
+    nose_y   = READ_INT16(face_data);
+    lmouth_x = READ_INT16(face_data);
+    lmouth_y = READ_INT16(face_data);
+    rmouth_x = READ_INT16(face_data);
+    rmouth_y = READ_INT16(face_data);
 }
 
 HandResult::HandResult(const void *buf) : Result(buf) {
@@ -85,55 +87,55 @@ HandResult::HandResult(const void *buf) : Result(buf) {
         return;
     }
 
-    wrist_x = *hand_data++; // 手腕
-    wrist_y = *hand_data++;
+    wrist_x = READ_INT16(hand_data); // 手腕
+    wrist_y = READ_INT16(hand_data);
     DBG_HEX(wrist_x);
     DBG_HEX(wrist_y);
-    thumb_cmc_x = *hand_data++;
-    thumb_cmc_y = *hand_data++; // 拇指根部
-    thumb_mcp_x = *hand_data++;
-    thumb_mcp_y = *hand_data++; // 拇指中间关节
-    thumb_ip_x  = *hand_data++;
-    thumb_ip_y  = *hand_data++; // 拇指第二关节
-    thumb_tip_x = *hand_data++;
-    thumb_tip_y = *hand_data++; // 拇指指尖
+    thumb_cmc_x = READ_INT16(hand_data);
+    thumb_cmc_y = READ_INT16(hand_data); // 拇指根部
+    thumb_mcp_x = READ_INT16(hand_data);
+    thumb_mcp_y = READ_INT16(hand_data); // 拇指中间关节
+    thumb_ip_x  = READ_INT16(hand_data);
+    thumb_ip_y  = READ_INT16(hand_data); // 拇指第二关节
+    thumb_tip_x = READ_INT16(hand_data);
+    thumb_tip_y = READ_INT16(hand_data); // 拇指指尖
 
-    index_finger_mcp_x  = *hand_data++;
-    index_finger_mcp_y  = *hand_data++; // 食指根部
-    index_finger_pip_x  = *hand_data++;
-    index_finger_pip_y  = *hand_data++; // 食指第一关节
-    index_finger_dip_x  = *hand_data++;
-    index_finger_dip_y  = *hand_data++; // 食指第二关节
-    index_finger_tip_x  = *hand_data++;
-    index_finger_tip_y  = *hand_data++; // 食指指尖
-    middle_finger_mcp_x = *hand_data++;
-    middle_finger_mcp_y = *hand_data++; // 中指根部
+    index_finger_mcp_x  = READ_INT16(hand_data);
+    index_finger_mcp_y  = READ_INT16(hand_data); // 食指根部
+    index_finger_pip_x  = READ_INT16(hand_data);
+    index_finger_pip_y  = READ_INT16(hand_data); // 食指第一关节
+    index_finger_dip_x  = READ_INT16(hand_data);
+    index_finger_dip_y  = READ_INT16(hand_data); // 食指第二关节
+    index_finger_tip_x  = READ_INT16(hand_data);
+    index_finger_tip_y  = READ_INT16(hand_data); // 食指指尖
+    middle_finger_mcp_x = READ_INT16(hand_data);
+    middle_finger_mcp_y = READ_INT16(hand_data); // 中指根部
 
-    middle_finger_pip_x = *hand_data++;
-    middle_finger_pip_y = *hand_data++; // 中指第一关节
-    middle_finger_dip_x = *hand_data++;
-    middle_finger_dip_y = *hand_data++; // 中指第二关节
-    middle_finger_tip_x = *hand_data++;
-    middle_finger_tip_y = *hand_data++; // 中指指尖
-    ring_finger_mcp_x   = *hand_data++;
-    ring_finger_mcp_y   = *hand_data++; // 无名指根部
-    ring_finger_pip_x   = *hand_data++;
-    ring_finger_pip_y   = *hand_data++; // 无名指第一关节
+    middle_finger_pip_x = READ_INT16(hand_data);
+    middle_finger_pip_y = READ_INT16(hand_data); // 中指第一关节
+    middle_finger_dip_x = READ_INT16(hand_data);
+    middle_finger_dip_y = READ_INT16(hand_data); // 中指第二关节
+    middle_finger_tip_x = READ_INT16(hand_data);
+    middle_finger_tip_y = READ_INT16(hand_data); // 中指指尖
+    ring_finger_mcp_x   = READ_INT16(hand_data);
+    ring_finger_mcp_y   = READ_INT16(hand_data); // 无名指根部
+    ring_finger_pip_x   = READ_INT16(hand_data);
+    ring_finger_pip_y   = READ_INT16(hand_data); // 无名指第一关节
 
-    ring_finger_dip_x  = *hand_data++;
-    ring_finger_dip_y  = *hand_data++; // 无名指第二关节
-    ring_finger_tip_x  = *hand_data++;
-    ring_finger_tip_y  = *hand_data++; // 无名指指尖
-    pinky_finger_mcp_x = *hand_data++;
-    pinky_finger_mcp_y = *hand_data++; // 小指根部
-    pinky_finger_pip_x = *hand_data++;
-    pinky_finger_pip_y = *hand_data++; // 小指第一关节
-    pinky_finger_dip_x = *hand_data++;
-    pinky_finger_dip_y = *hand_data++; // 小指第二关节
+    ring_finger_dip_x  = READ_INT16(hand_data);
+    ring_finger_dip_y  = READ_INT16(hand_data); // 无名指第二关节
+    ring_finger_tip_x  = READ_INT16(hand_data);
+    ring_finger_tip_y  = READ_INT16(hand_data); // 无名指指尖
+    pinky_finger_mcp_x = READ_INT16(hand_data);
+    pinky_finger_mcp_y = READ_INT16(hand_data); // 小指根部
+    pinky_finger_pip_x = READ_INT16(hand_data);
+    pinky_finger_pip_y = READ_INT16(hand_data); // 小指第一关节
+    pinky_finger_dip_x = READ_INT16(hand_data);
+    pinky_finger_dip_y = READ_INT16(hand_data); // 小指第二关节
     DBG_HEX(pinky_finger_dip_x);
     DBG_HEX(pinky_finger_dip_y);
-    pinky_finger_tip_x = *hand_data++;
-    pinky_finger_tip_y = *hand_data++; // 小指指尖
+    pinky_finger_tip_x = READ_INT16(hand_data);
+    pinky_finger_tip_y = READ_INT16(hand_data); // 小指指尖
     DBG_HEX(pinky_finger_tip_x);
     DBG_HEX(pinky_finger_tip_y);
 }
@@ -155,40 +157,40 @@ PoseResult::PoseResult(const void *buf) : Result(buf) {
     }
     DBG("hax externed data");
     DBG_PRINTLN(head->data_length - (uint8_t)((uint32_t)pose_data - (uint32_t)packet));
-    nose_x      = *pose_data++;
-    nose_y      = *pose_data++;
-    leye_x      = *pose_data++;
-    leye_y      = *pose_data++;
-    reye_x      = *pose_data++;
-    reye_y      = *pose_data++;
-    lear_x      = *pose_data++;
-    lear_y      = *pose_data++;
-    rear_x      = *pose_data++;
-    rear_y      = *pose_data++;
-    lshoulder_x = *pose_data++;
-    lshoulder_y = *pose_data++;
-    rshoulder_x = *pose_data++;
-    rshoulder_y = *pose_data++;
-    lelbow_x    = *pose_data++;
-    lelbow_y    = *pose_data++;
-    relbow_x    = *pose_data++;
-    relbow_y    = *pose_data++;
-    lwrist_x    = *pose_data++;
-    lwrist_y    = *pose_data++;
-    rwrist_x    = *pose_data++;
-    rwrist_y    = *pose_data++;
-    lhip_x      = *pose_data++;
-    lhip_y      = *pose_data++;
-    rhip_x      = *pose_data++;
-    rhip_y      = *pose_data++;
-    lknee_x     = *pose_data++;
-    lknee_y     = *pose_data++;
-    rknee_x     = *pose_data++;
-    rknee_y     = *pose_data++;
-    lankle_x    = *pose_data++;
-    lankle_y    = *pose_data++;
-    rankle_x    = *pose_data++;
-    rankle_y    = *pose_data++;
+    nose_x      = READ_INT16(pose_data);
+    nose_y      = READ_INT16(pose_data);
+    leye_x      = READ_INT16(pose_data);
+    leye_y      = READ_INT16(pose_data);
+    reye_x      = READ_INT16(pose_data);
+    reye_y      = READ_INT16(pose_data);
+    lear_x      = READ_INT16(pose_data);
+    lear_y      = READ_INT16(pose_data);
+    rear_x      = READ_INT16(pose_data);
+    rear_y      = READ_INT16(pose_data);
+    lshoulder_x = READ_INT16(pose_data);
+    lshoulder_y = READ_INT16(pose_data);
+    rshoulder_x = READ_INT16(pose_data);
+    rshoulder_y = READ_INT16(pose_data);
+    lelbow_x    = READ_INT16(pose_data);
+    lelbow_y    = READ_INT16(pose_data);
+    relbow_x    = READ_INT16(pose_data);
+    relbow_y    = READ_INT16(pose_data);
+    lwrist_x    = READ_INT16(pose_data);
+    lwrist_y    = READ_INT16(pose_data);
+    rwrist_x    = READ_INT16(pose_data);
+    rwrist_y    = READ_INT16(pose_data);
+    lhip_x      = READ_INT16(pose_data);
+    lhip_y      = READ_INT16(pose_data);
+    rhip_x      = READ_INT16(pose_data);
+    rhip_y      = READ_INT16(pose_data);
+    lknee_x     = READ_INT16(pose_data);
+    lknee_y     = READ_INT16(pose_data);
+    rknee_x     = READ_INT16(pose_data);
+    rknee_y     = READ_INT16(pose_data);
+    lankle_x    = READ_INT16(pose_data);
+    lankle_y    = READ_INT16(pose_data);
+    rankle_x    = READ_INT16(pose_data);
+    rankle_y    = READ_INT16(pose_data);
     DBG("end parse\n");
 }
 
